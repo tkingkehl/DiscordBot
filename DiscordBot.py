61b8a280a4cf206d5eea4749937dcaf1b0fb8df3
Tim King-Kehl <acanceroustwzlr@gmail.com>
2017-11-27 08:34:00 -0600

    Added !meme and formatting changes.

--- DiscordBot.py
+++ DiscordBot.py
@@ -14,20 +14,22 @@ async def on_ready():
     print(client.user.id)
     print('-------')
 
-    # TEST
-
 @client.event
 async def on_message(message):
     if message.content.startswith('!istimcool'): # Tim is cool, duh.
         await client.send_message(message.channel, 'Well of course.')
-    if message.content.startswith('!hidrricks'): # Tim is cool, duh.
+
+    elif message.content.startswith('!hidrricks'): # Tim is cool, duh.
         await client.send_message(message.channel, 'Hello! It must be lab day.')
+
     elif message.content.startswith('!whattimeisit'): # Returns current time
         await client.send_message(message.channel, datetime.datetime.now().time())
         await client.send_message(message.channel, 'Central Time')
+
     elif message.content.startswith('!coinflip'): # Flips a coin
         resultOfFlip = random.choice(['Heads', 'Tails'])
         await client.send_message(message.channel, resultOfFlip)
+
     elif message.content.startswith('!addquote'): # Adds a quote to a running quote file.!
         if not os.path.isfile("quote_file.pk1"):
             quote_list = []
@@ -37,13 +39,23 @@ async def on_message(message):
         quote_list.append(message.content[9:])
         with open("quote_file.pk1", "w") as quote_file:
                 json.dump(quote_list, quote_file)
+
     elif message.content.startswith('!quote'): # Pulls a random quote from the quote file.
         with open("quote_file.pk1", "r") as quote_file:
                 quote_list = json.load(quote_file)
         await client.send_message(message.channel, random.choice(quote_list))
+
     elif message.content.startswith('!rps'): # Play rock paper scissors.
         rps = random.choice(["Rock", "Paper", "Scissors"])
         await client.send_message(message.channel, rps)
+
+    elif message.content.startswith('!meme'):  # Turns to emoji text.
+        Message = message.content[6:]
+
+        if Message == 'hello':
+            await client.send_message(message.channel, ':regional_indicator_h::regional_indicator_e::regional_indicator_l::regional_indicator_l::regional_indicator_o:')
+        elif Message == 'liam sucks':
+            await client.send_message(message.channel, ':regional_indicator_l::regional_indicator_i::regional_indicator_a::regional_indicator_m: :regional_indicator_s::regional_indicator_u::regional_indicator_c::regional_indicator_k::regional_indicator_s:')
 
 file = open("C:/Users/Tim/Documents/token.txt")
 token = file.read()
